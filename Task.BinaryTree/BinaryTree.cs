e4713d512b32f9253174bc4ab328f90a7dca145e
kirill-vinnichek <kirill.vinnichek@gmail.com>
2015-11-12 13:55:35 +0300

    Task is done

---
+++ Task.BinaryTree/BinaryTree.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.BinaryTree
+{
+    public sealed class BinaryTree<T> : IEnumerable<T>
+    {
+        private class Node<T>
+        {
+            public T Value { get;  set; }
+            public Node<T> Right { get; set; }
+            public Node<T> Left { get; set; }
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private Node<T> root;
+        public int Count { get; private set; }
+        public IComparer<T> Comparer;
+        public T Min
+        {
+            get
+            {
+                if (root == null)
+                    throw new InvalidOperationException();
+                var current = root;
+                while (current.Left != null)
+                    current = current.Left;
+                return current.Value;
+            }
+        }
+        public T Max
+        {
+            get
+            {
+                if (root == null)
+                    throw new InvalidOperationException();
+                var current = root;
+                while (current.Right != null)
+                    current = current.Right;
+                return current.Value;
+            }
+        }
+
+        public BinaryTree()
+            : this(Comparer<T>.Default)
+        { }
+
+        public BinaryTree(IComparer<T> comparer)
+        {
+            if (comparer == null)
+                this.Comparer = Comparer<T>.Default;
+            else
+                this.Comparer = comparer;
+            if (Comparer == null)
+                throw new ArgumentException("Comparer is null");
+        }
+
+        public void Add(T value)
+        {
+            var node = new Node<T>(value);
+            int result;
+            if (root == null)
+                root = node;
+            else
+            {
+                Node<T> current = root;
+                Node<T> parent = null;
+                do
+                {
+                    parent = current;
+                    result = Comparer.Compare(value, current.Value);
+                    current = result < 0 ? current.Left : current.Right;
+                }
+                while (current != null);
+                if (result < 0)
+                    parent.Left = node;
+                else
+                    parent.Right = node;
+            }
+            Count++;
+        }
+        public bool Remove(T value)
+        {
+            if (root == null)
+                return false;
+            var current = root;
+            Node<T> parent = null;
+            while (current != null)
+            {
+                int result = Comparer.Compare(value, current.Value);
+                if (result == 0)
+                    break;
+                parent = current;
+                current = result < 0 ? current.Left : current.Right;
+            }
+            if (current == null)
+                return false;
+            return Remove(current, parent);
+
+        }
+        private bool Remove(Node<T> current, Node<T> parent)
+        {
+                Node<T> removed = null;
+                if (current.Left == null || current.Right == null)
+                {
+                    Node<T> child = null;
+                    removed = current;
+
+                    if (current.Left != null)
+                        child = current.Left;
+                    else if (current.Right != null)
+                        child = current.Right;
+
+                    if (parent == null)
+                        root = child;
+                    else
+                    {
+                        if (parent.Left == current)
+                            parent.Left = child;
+                        else
+                            parent.Right = child;
+                    }
+                }
+                else 
+                {
+                    Node<T> mostLeft = current.Right;
+                    Node<T> mostLeftParent = current;
+
+                    while (mostLeft.Left != null)
+                    {
+                        mostLeftParent = mostLeft;
+                        mostLeft = mostLeft.Left;
+                    }
+
+                    current.Value = mostLeft.Value;
+                    removed = mostLeft;
+
+                    if (mostLeftParent.Left == mostLeft)
+                        mostLeftParent.Left = null;
+                    else
+                        mostLeftParent.Right = mostLeft.Right;
+                }
+
+                Count--;
+                return true;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Inorder(root).GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerable<T> Preorder()
+        {
+            return Preorder(root);
+        }
+        private IEnumerable<T> Preorder(Node<T> node)
+        {
+            if (node == null)
+                yield break;
+            yield return node.Value;
+            foreach (var e in Preorder(node.Left))
+                yield return e;
+
+            foreach (var e in Preorder(node.Right))
+                yield return e;
+        }
+        public IEnumerable<T> Inorder()
+        {
+            return Inorder(root);
+        }
+        private IEnumerable<T> Inorder(Node<T> node)
+        {
+            if (node == null)
+                yield break;
+            foreach (var e in Preorder(node.Left))
+                yield return e;
+            yield return node.Value;
+            foreach (var e in Preorder(node.Right))
+                yield return e;
+        }
+        public IEnumerable<T> Postorder()
+        {
+            return Postorder(root);
+        }
+        private IEnumerable<T> Postorder(Node<T> node)
+        {
+            if (node == null) 
+                yield break;
+            foreach (var e in Preorder(node.Left))
+                yield return e;
+            foreach (var e in Preorder(node.Right))
+                yield return e;
+            yield return node.Value;
+        } 
+    }
+}
---
+++ Task.Test/UnitTest1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Task1.Book;
+namespace Task.Test
+{
+    public struct Point2D {
+        public int x;
+        public int y;
+        public Point2D(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class IntComparer : IComparer<int> {
+        public int Compare(int a, int b)
+        {
+            return Math.Abs(a).ToString().Length - Math.Abs(b).ToString().Length;
+        }
+
+    }
+    public class StringComparer : IComparer<string>
+    {
+        public int Compare(string a, string b)
+        {
+            return a.Length - b.Length;
+        }
+
+    }
+
+    public class BookComparer : IComparer<Book>
+    {
+        public int Compare(Book a, Book b)
+        {
+            return a.Series - b.Series;
+        }
+
+    }
+    [TestFixture]
+    public class UnitTest1
+    {
+        private IEnumerable<TestCaseData> IntTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {15,23,17,-23,123,1,103 }, null, -23,123);
+                yield return new TestCaseData(new[] { 15, 23, 17, -23, 123, 1, 103 }, new IntComparer(), 1, 103);
+            }
+        }
+        [Test,TestCaseSource("IntTestData")]
+        public void IntTest(int[] array,IComparer<int> comparer,int min,int max)
+        {
+            var tree = new BinaryTree.BinaryTree<int>(comparer);
+            foreach (var i in array)
+            {
+                tree.Add(i);
+            }
+            Assert.AreEqual(tree.Max, max);
+            Assert.AreEqual(tree.Min, min);
+        }
+
+        private IEnumerable<TestCaseData> StringTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new[] {"p","AB","qwesas","Oisks" ,"ZZZ"}, null, "AB", "ZZZ");
+                yield return new TestCaseData(new[] { "p", "AB", "qwesaswe", "Oisks", "ZZZ" }, new StringComparer(), "p", "qwesaswe");
+            }
+        }
+        [Test, TestCaseSource("StringTestData")]
+        public void StringTest(string[] array, IComparer<string> comparer, string min, string max)
+        {
+            var tree = new BinaryTree.BinaryTree<string>(comparer);
+            foreach (var i in array)
+            {
+                tree.Add(i);
+            }
+            Assert.AreEqual(tree.Max, max);
+            Assert.AreEqual(tree.Min, min);
+        }
+
+        private IEnumerable<TestCaseData> BookTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new[]  {new Book("Albahari",@"C# in Nutshell",100,"ru"),                         
+                             new Book("Esposito",@"ASP .NET MVC 4",1,"ru"),
+                                 new Book("Albahari",@"C# in Nutshell",3,"en"),
+                             new Book("Фримен",@"Паттерны проектирования",15,"ru")
+                            }, 
+                            null, new Book("Albahari", @"C# in Nutshell", 3, "en"), new Book("Фримен", @"Паттерны проектирования", 15, "ru"));
+
+                yield return new TestCaseData(new[]{new Book("Albahari",@"C# in Nutshell",100,"ru"),                         
+                             new Book("Esposito",@"ASP .NET MVC 4",1,"ru"),
+                                 new Book("Albahari",@"C# in Nutshell",3,"en"),
+                             new Book("Фримен",@"Паттерны проектирования",15,"ru")
+                            }, new BookComparer(), new Book("Esposito", @"ASP .NET MVC 4", 1, "ru"), new Book("Albahari",@"C# in Nutshell",100,"ru"));
+            }
+        }
+        [Test, TestCaseSource("BookTestData")]
+        public void BookTest(Book[] array, IComparer<Book> comparer, Book min, Book max)
+        {
+            var tree = new BinaryTree.BinaryTree<Book>(comparer);
+            foreach (var i in array)
+            {
+                tree.Add(i);
+            }
+            Assert.AreEqual(tree.Max, max);
+            Assert.AreEqual(tree.Min, min);
+        }
+
+        private IEnumerable<TestCaseData> PointTestData
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { new Point2D(1, 2), new Point2D(5, 6), new Point2D(-2, -12), new Point2D(8, 0) }, null, new Point2D(1, 2), new Point2D(-2, -12));
+            }
+        }
+        [Test, TestCaseSource("PointTestData")]
+        public void PointTest(Point2D[] array, IComparer<Point2D> comparer, Point2D min, Point2D max)
+        {
+            var tree = new BinaryTree.BinaryTree<Point2D>(comparer);
+            foreach (var i in array)
+            {
+                tree.Add(i);
+            }
+            Assert.AreEqual(tree.Max, max);
+            Assert.AreEqual(tree.Min, min);
+        }
+    }
+}
